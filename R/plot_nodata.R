99ecc12cbf29802d4cd0aa636b8a2cdc67805b48
Alexis Roldan <argos2416@gmail.com>
2022-07-21 23:29:00 -0700

    fix dependencies

--- R/plot_nodata.R
+++ R/plot_nodata.R
@@ -2,7 +2,7 @@
 #'
 #' This function will show a message when there's no data to populate a plot
 #'
-#' @importFrom plotly plot_ly
+#' @importFrom plotly plot_ly layout config
 #' @import dplyr
 #' @param height_plot value showing the height of the plot.
 #' @param message message to be used by the function when data is not available.
@@ -10,14 +10,17 @@
 #' @export
 
 plot_nodata <- function(height_plot = 450, message = "No data available", text_size = 20) {
+
   text_na <- list(x = 5, y = 5, text =  message, size = text_size,
                   xref = "x", yref = "y",  showarrow = FALSE)
 
   plotly::plot_ly(height = height_plot) %>%
-    layout(annotations = text_na,
-           #empty layout
-           yaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
-           xaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
-           font = list(family = '"Helvetica Neue", Helvetica, Arial, sans-serif')) %>%
-    config( displayModeBar = FALSE) # taking out plotly logo and collaborate button
+    plotly::layout(
+      annotations = text_na,
+      #empty layout
+      yaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
+      xaxis = list(showline = FALSE, showticklabels = FALSE, showgrid = FALSE, fixedrange=TRUE),
+      font = list(family = '"Helvetica Neue", Helvetica, Arial, sans-serif')) %>%
+    plotly::config(displayModeBar = FALSE) # taking out plotly logo and collaborate button
+
   }
